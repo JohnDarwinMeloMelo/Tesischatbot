acc8401a8eec5b5f8791d49663f5126ab47eda06
JohnDarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2023-09-26 18:34:58 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, request
+
+from heyoo import WhatsApp
+import json
+import random
+import json
+import pickle
+import numpy as np
+import torch
+import nltk
+import unidecode
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+import os
+from langdetect import detect 
+
+app = Flask(__name__)
+
+# Ruta para recibir las solicitudes de WhatsApp
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    if request.method == "GET":
+        if request.args.get('hub.verify_token') == "chatbotvbg":
+            return request.args.get('hub.challenge')
+        else:
+            return "Error de autenticacion."
+
+    data = request.get_json()
+
+    telefonoCliente = None
+    mensaje = None
+    idWA = None
+    timestamp = None
+
+    if 'entry' in data and data['entry']:
+        entry = data['entry'][0]
+        if 'changes' in entry and entry['changes']:
+            change = entry['changes'][0]
+            if 'value' in change and 'messages' in change['value'] and change['value']['messages']:
+                telefonoCliente = change['value']['messages'][0]['from']
+                mensaje = change['value']['messages'][0]['text']['body']
+                idWA = change['value']['messages'][0]['id']
+                timestamp = change['value']['messages'][0]['timestamp']
+
+    if mensaje is not None:
+        
+        
+        lemmatizer = WordNetLemmatizer()
+
+        
+        # Abre el archivo JSON con la codificación UTF-8
+        with open('intents.json', 'r', encoding='utf-8') as file:
+            file_content = file.read()
+
+        # Carga el contenido del archivo JSON en un objeto Python
+        intents = json.loads(file_content)
+        
+        
+        #intents = json.loads(open('intents.json').read())
+        words = pickle.load(open('words.pkl', 'rb'))
+        classes = pickle.load(open('classes.pkl', 'rb'))
+        model = load_model('chatbot_model.h5')
+
+        #Pasamos las palabras de oración a su forma raíz
+        def clean_up_sentence(sentence):
+            sentence_words = nltk.word_tokenize(sentence)
+            sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+            return sentence_words
+
+        #Convertimos la información a unos y ceros según si están presentes en los patrones
+        def bag_of_words(sentence):
+            sentence_words = clean_up_sentence(sentence)
+            bag = [0]*len(words)
+            for w in sentence_words:
+                for i, word in enumerate(words):
+                    if word == w:
+                        bag[i]=1
+            print(bag)
+            return np.array(bag)
+
+        
+        #Predecimos la categoría a la que pertenece la oración
+        def predict_class(sentence):
+            bow = bag_of_words(sentence)
+            
+            
+            ####################
+            has_ones = np.any(bow)
+
+            if has_ones:
+                res = model.predict(np.array([bow]))[0]
+                max_index = np.where(res ==np.max(res))[0][0]
+                category = classes[max_index]
+            else:
+                category = "desconocido"
+            print(bow)        
+            print(category)
+            return category
+        
+
+        
+        
+        #Obtenemos una respuesta aleatoria
+        def get_response(tag, intents_json):
+            list_of_intents = intents_json['intents']
+            result = ""
+            for i in list_of_intents:
+                if i["tag"]==tag:
+                    result = random.choice(i['responses'])
+                    break
+            return result
+
+        #-------------------------------------------
+        #Dectectar palabra mal escrita ----------------------------------------------------------------
+
+        
+        
+        #----------------------------------------INICIO DE MENSAJE 
+        
+        
+        
+        mensaje = unidecode.unidecode(mensaje.lower())
+
+        
+        ints = predict_class(mensaje)
+   
+
+        
+        
+        
+       
+        
+        respuesta = get_response(ints, intents)
+        
+        
+    
+
+        with open("texto.txt", "w", encoding="utf-8") as f:
+            f.write(respuesta)
+
+
+
+        enviar(telefonoCliente, respuesta)
+
+        return jsonify({"status": "success"}, 200)
+    
+    else:
+        # En caso de que no haya mensaje, puedes retornar una respuesta de error
+        return jsonify({"status": "error", "message": "No se recibió un mensaje válido"}, 400)
+
+def enviar(telefonoRecibe, respuesta):
+    token = 'EAALZBZB8exxfkBO1dKOrQIR2e3nZCNK9ZANZCo7VSkInr1aepGYWrvM3jSgjGnnwejZAkXaFrwNwRn4zzZCSP8hLhzr9QcSh3bAEKMbxxkVX8msLPGqDZAAtpeGhZBvQZCv5JVLA0MfJQQyRPu8Rt2NEkMoZCXIMs4qqxAHmGMvEc8k7PnaLO0EiVF7PayuTKRZC'  # Reemplaza con tu token de WhatsApp
+    idNumeroTelefono = '117836178073425'  # Reemplaza con tu ID de número de teléfono
+
+    mensajeWa = WhatsApp(token, idNumeroTelefono)
+    telefonoRecibe = telefonoRecibe.replace("521", "52")
+    
+    mensajeWa.send_message(respuesta, telefonoRecibe)
+
+if __name__ == "__main__":
+    app.config['JSON_AS_ASCII'] = False
+    
+    app.run(host='0.0.0.0', port=5000)
---
+++ training.py
@@ -0,0 +1,105 @@
+import random
+import json
+import pickle
+import numpy as np
+import unidecode
+import nltk
+from nltk.stem import WordNetLemmatizer #Para pasar las palabras a su forma raíz
+from nltk.corpus import stopwords
+#Para crear la red neuronal
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+
+#from keras.optimizers import sgd_experimental
+#from keras.optimizers import gradient_descent_v2 
+from keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+
+# Abre el archivo JSON con la codificación UTF-8
+with open('intents.json', 'r', encoding='utf-8') as file:
+    file_content = file.read()
+
+# Carga el contenido del archivo JSON en un objeto Python
+intents = json.loads(file_content)
+
+# intents = json.loads(open('intents.json').read())
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+
+stop_words = set(stopwords.words('spanish'))  # Lista de stopwords en spanish
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '¿', '.', ',']
+
+#Clasifica los patrones y las categorías
+for intent in intents['intents']:
+    
+    for pattern in intent['patterns']:
+        pattern = unidecode.unidecode(pattern.lower())
+        word_list = nltk.word_tokenize(pattern)
+        # Filtramos stopwords y lematizamos
+        word_list = [lemmatizer.lemmatize(word) for word in word_list if word not in ignore_letters and word not in stop_words]
+        words.extend(word_list)
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+words = sorted(set(words))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+#Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
+training = []
+output_empty = [0]*len(classes)
+
+
+for document in documents:
+    bag = [0] * len(words)  # Inicializar bag con ceros
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in word_patterns:
+        if word in words:
+            bag[words.index(word)] = 1  # Cambiar el valor en la posición correspondiente
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+######################################################################## 
+ 
+    
+random.shuffle(training)
+#training = np.array(training) 
+train_x = np.array([i[0] for i in training])
+train_y = np.array([i[1] for i in training])
+print(training) 
+
+#Reparte los datos para pasarlos a la red
+#train_x = list(training[:,0])
+#train_y = list(training[:,1])
+
+#Creamos la red neuronal
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+#Creamos el optimizador y lo compilamos
+
+#sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
+
+model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+#Entrenamos el modelo y lo guardamos
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+model.save("chatbot_model.h5", train_process)

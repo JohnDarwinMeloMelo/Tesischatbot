4119c4f1bec8441f6c879e9d5b2fae8dfb835196
JohnDarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2023-09-26 21:39:06 -0500

    Chatbot v2

--- training.py
+++ training.py
@@ -44,13 +44,13 @@ for intent in intents['intents']:
     for pattern in intent['patterns']:
         pattern = unidecode.unidecode(pattern.lower())
         word_list = nltk.word_tokenize(pattern)
-        # Filtramos stopwords y lematizamos
-        word_list = [lemmatizer.lemmatize(word) for word in word_list if word not in ignore_letters and word not in stop_words]
+        word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent["tag"]))
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 pickle.dump(words, open('words.pkl', 'wb'))
